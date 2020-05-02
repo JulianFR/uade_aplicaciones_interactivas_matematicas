6d2fedbec2cdd0c7348ee0ff2674e169b79739da
unknown <joct89@gmail.com>
2020-05-02 15:53:04 -0300

    +login responsive

---
+++ src/Principal/Principal.component.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default () => 
+  <div className="principal">
+    <p>Bienvenido Max</p>
+    <p>¿Qué te gustaría practicar hoy?</p>
+    <ul>
+      <li>
+        <img src="" alt=""/>
+        <p>Sumas y Restas</p>
+        <p>Tu mejor puntuación: 0</p>
+        </li>
+      <li>
+      <img src="" alt=""/>
+        <p>Juego 2</p>
+        <p>Tu mejor puntuación: 0</p>
+      </li>
+      <li>
+      <img src="" alt=""/>
+        <p>Juego 3</p>
+        <p>Tu mejor puntuación: 0</p>
+      </li>
+    </ul>
+    <p>Mejores Puntuaciones</p>
+  </div>;
