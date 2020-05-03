91762ceab928916fe3a3ef413c9b3f795e86f421
unknown <joct89@gmail.com>
2020-05-03 14:24:30 -0300

    +mock pantalla principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import SumaRestaComponent from './Juego/SumaResta.component';
 import AccesoComponent from './Acceso/Acceso.component';
+import PrincipalComponent from './Principal/Principal.component';
 
 export default () =>
-  <AccesoComponent></AccesoComponent>
-//    <SumaRestaComponent></SumaRestaComponent>
+  //<AccesoComponent></AccesoComponent>
+  //<SumaRestaComponent></SumaRestaComponent>
+  <PrincipalComponent></PrincipalComponent>
--- src/Principal/Principal.component.tsx
+++ src/Principal/Principal.component.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import './Principal.css';
+import '../App.css';
 
 export default () => 
-  <div className="principal">
-    <p>Bienvenido Max</p>
-    <p>¿Qué te gustaría practicar hoy?</p>
-    <ul>
-      <li>
-        <img src="" alt=""/>
-        <p>Sumas y Restas</p>
-        <p>Tu mejor puntuación: 0</p>
+  <div className="principal ">
+    <h2 className="principal__titulo">Bienvenido</h2>
+    <div className="principal__avatar"></div>
+    <h1 className="principal__nombre">Max</h1>
+    <h3 className="principal__subtitulo">¿Qué te gustaría practicar hoy?</h3>
+    <ul className="principal__juegos">
+      <li className="principal__juego ">
+        <h4>Sumas y Restas</h4>
+        <img className="principal__juego-imagen" src="" alt=""/>
+        <p>Tu mejor puntuación</p>
+        <p>0</p>
         </li>
-      <li>
-      <img src="" alt=""/>
-        <p>Juego 2</p>
-        <p>Tu mejor puntuación: 0</p>
+      <li className="principal__juego ">
+        <h4>Juego 2</h4>
+      <img className="principal__juego-imagen" src="" alt=""/>
+        <p>Tu mejor puntuación</p>
+        <p>0</p>
       </li>
-      <li>
-      <img src="" alt=""/>
-        <p>Juego 3</p>
-        <p>Tu mejor puntuación: 0</p>
+      <li className="principal__juego ">
+        <h4>Juego 3</h4>
+      <img className="principal__juego-imagen" src="" alt=""/>
+        <p>Tu mejor puntuación</p>
+        <p>0</p>
       </li>
     </ul>
-    <p>Mejores Puntuaciones</p>
+    <p className="principal__opcion">Mejores Puntuaciones</p>
+    <p className="principal__opcion">Salir</p>
   </div>;
